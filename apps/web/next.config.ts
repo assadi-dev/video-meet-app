4bc3bfb53fd8a693313e8618bf1f80b13559970f
assadi-dev <assadi.halifa@gmail.com>
2026-05-14 14:54:53 +0200

    feat: initialize web application workspace and upgrade minimum Node.js version to 24

---
+++ apps/web/next.config.ts
@@ -0,0 +1,31 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: false,
+  turbopack: {
+    resolveExtensions: [
+      ".css",
+      ".mdx",
+      ".tsx",
+      ".ts",
+      ".jsx",
+      ".js",
+      ".mjs",
+      ".json",
+      ".mp4",
+      ".webm",
+      ".ogg",
+      ".mp3",
+      ".wave",
+      ".aac",
+      ".rtf",
+      ".doc",
+      ".docx",
+    ],
+  },
+};
+
+export default nextConfig;
