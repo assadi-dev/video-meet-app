ad84fdabd307845e8074b2e89be6a3ba7127174e
assadi-dev <assadi.halifa@gmail.com>
2026-05-15 22:18:40 +0200

    feat: define dependency injection tokens for server and livekit service

--- apps/server/src/injection/SignalingServer.injection.ts
+++
@@ -1,7 +0,0 @@
-import { container } from "tsyringe";
-import { ExpressServer } from "@core/interfaces/global";
-import { TOKENS } from "./tokens";
-
-export const registerServerDependencies = (server: ExpressServer) => {
-  container.registerInstance<ExpressServer>(TOKENS.server, server);
-};
--- apps/server/src/injection/tokens.ts
+++ apps/server/src/injection/tokens.ts
@@ -1,5 +1,4 @@
 export const TOKENS = {
   server: "server",
-  mediasoupServices: "mediasoup-services",
   liveKitService: "livekit-service",
 };
