66832c5690502f639761934c09eca061b64a656a
assadi-dev <assadi.halifa@gmail.com>
2026-05-18 01:12:36 +0200

    feat: initialize server entry point and implement environment variable validation with Turbo configuration updates

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,12 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config();
-
+import "dotenv/config";
 
 // Enable decorators
 import "reflect-metadata";
-
-// Enable env config
-import "dotenv/config";
 
 import "./core/config/env";
 
